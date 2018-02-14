1e4b28509437f547248b6db5e0199c3b6076e66c
qwedsazxc78 <qwedsazxc78@gmail.com>
2018-02-14 12:10:56 +0800

    add category

---
+++ maiziedu/ch12.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+	int i = 0;
+	printf("-- 1. show type by array --" );
+	for ( i = 0; i < argc; i++)
+	{
+		printf("argv[%d]: %s\n", i, argv[i] );
+	}
+
+	printf("-- 2. show type by point --" );
+	i = 0;
+	while (argv[i] != NULL) {
+		printf("argv: %s\n", argv[i]);
+		i++;
+	}
+
+
+	return 0;
+}
